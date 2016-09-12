757112f853ff3f9e80a652672444770e95435032
leoadvance <leo_advance@163.com>
2016-09-13 00:26:47 +0800

    串口 接收

--- ArduinoMega2560.ino
+++ ArduinoMega2560.ino
@@ -1,14 +1,49 @@
-void setup() 
+
+
+String inputString = "";         // a string to hold incoming data
+boolean stringComplete = false;  // whether the string is complete
+
+
+void setup()
 {
   // put your setup code here, to run once:
   Serial.begin(115200);
 
 }
 
-void loop() 
+void loop()
 {
-  
-  delay(1000);
-  Serial.println("Hello Mega2560");
+
+  //  delay(1000);
+  //  Serial.println("Hello Mega2560");
+
+  if (stringComplete)
+  {
+    Serial.print("收到数据:");
+    Serial.println(inputString);
+    // clear the string:
+    inputString = "";
+    stringComplete = false;
+  }
 
 }
+
+
+void serialEvent()
+{
+  while (Serial.available())
+  {
+    // get the new byte:
+    char inChar = (char)Serial.read();
+    // add it to the inputString:
+    inputString += inChar;
+    // if the incoming character is a newline, set a flag
+    // so the main loop can do something about it:
+    if (inChar == '\n')
+    {
+      stringComplete = true;
+    }
+  }
+}
+
+
